440b8ce3651fb2454a35417ca68802f820565622
eg15 <gladchenko@gmail.com>
2018-11-05 01:07:15 +0000

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,5 +1,9 @@
 class profile::agent_nodes {
   include dockeragent
-  dockeragent::node {'web.puppet.vm':}
-  dockeragent::node {'db.puppet.vm':}
+  dockeragent::node {'web.puppet.vm':
+    ensure => running,
+  }
+  dockeragent::node {'db.puppet.vm':
+    ensure => running,
+  }
 }
