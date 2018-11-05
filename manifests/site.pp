8f0acc7cbafd9c8d5a732af3d4b99ab9df854178
eg15 <gladchenko@gmail.com>
2018-11-05 01:14:39 +0000

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,9 +1,4 @@
 node default {
-#  file {'/root/README':
-#    ensure  => file,
-#    content => 'This is a readme file.',
-#    owner   => 'root',
-#  }
 }
 node 'master.puppet.vm' {
   include role::master_server
